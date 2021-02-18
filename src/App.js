edc952fd68de7c9e6a3d5285c77fb7122c0c63ec
Subhrajyoti Mondal <subhrajyoti.mondol@gmail.com>
2021-02-18 14:57:27 -0800

    components and props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
 
@@ -11,11 +12,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a sunny2 App</h1>
-       <h2>{this.add(10, 20)}</h2> 
+        <h1>Hi, I'm a Sunny App</h1>
+         <h2>10 + 20 = {this.add(10, 20)}</h2> 
+         <p>Another para</p>
+
+          <Person name="Subhra" age="22"/>
+          <Person name="Somnath" age="53">My Hobbies: Programing</Person> 
+          <Person name="Byleth" age="25"/>
+          <Person/>
+
       </div>
 
     );
+
+    // Really cumbersome way to do it
+    // return React.creatElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+
   }
 }
 
